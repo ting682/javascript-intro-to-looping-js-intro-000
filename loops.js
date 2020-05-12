bba2f53a5ba02824ccc1cd7e877b903283dd2f63
Ting Chung <tchung682@gmail.com>
2020-05-12 18:25:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,5 @@
 function forLoop(array){
-  for (var i = 0; i < 25; i++) {
-    array[i] = `I am ${i} strange loop${i === 0 ? '': 's'}.`;
-
+  for (let i = 0; i < 25; i++) {
+    array[i] = `I am ${i+1} strange loop${i === 0 ? '': 's'}.`;
   }
-
 }
