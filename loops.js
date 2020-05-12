94c6ab074566443868ef72f24bc870c417aea3d4
Ting Chung <tchung682@gmail.com>
2020-05-12 20:18:38 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,8 +12,7 @@ function forLoop(array){
   var k = 1
   for (let j = 10; j<25; j++){
 
-
-    array[j] = `I am ${k} strange loop${k === 0 ? '':'s'}.`
+    array[j] = `I am ${k} strange loop${k === 1 ? '':'s'}.`
     k = k + 1;
     console.log(array[j])
   }
